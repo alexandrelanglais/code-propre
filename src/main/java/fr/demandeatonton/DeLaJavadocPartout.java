1507874216f72740bec560fbd5695e7abd3d5749
Alexandre Langlais <alanglaispro@gmail.com>
2017-07-29 11:24:37 +0200

    add javadoc everywhere

--- src/main/java/fr/demandeatonton/DeLaJavadocPartout.java
+++ src/main/java/fr/demandeatonton/DeLaJavadocPartout.java
@@ -1,11 +1,101 @@
 package fr.demandeatonton;
 
 public class DeLaJavadocPartout {
-    String firstName;
-    String lastName;
-    int age;
-    int height;
-    int weight;
-    int sex;
+	String firstName;
+	String lastName;
+	int age;
+	int height;
+	int weight;
+	int sex;
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age
+	 *            the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the height
+	 */
+	public int getHeight() {
+		return height;
+	}
+
+	/**
+	 * @param height
+	 *            the height to set
+	 */
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	/**
+	 * @return the weight
+	 */
+	public int getWeight() {
+		return weight;
+	}
+
+	/**
+	 * @param weight
+	 *            the weight to set
+	 */
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	/**
+	 * @return the sex
+	 */
+	public int getSex() {
+		return sex;
+	}
+
+	/**
+	 * @param sex
+	 *            the sex to set
+	 */
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
 
 }
